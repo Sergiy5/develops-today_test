e236161b41df28e0e533c02b66ab5277c9570dd7
Serhii Kushnir <s1983kushnir@gmail.com>
2025-12-10 15:21:23 +0100

    Init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -6,38 +9,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const compat = new FlatCompat({ baseDirectory: __dirname });
 
-export default [
-  // Extend Next.js recommended rules
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-
-  // Prettier integration
-  {
-    plugins: {
-      prettier: require('eslint-plugin-prettier'),
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
+export default [// Extend Next.js recommended rules
+...compat.extends('next/core-web-vitals', 'next/typescript'), // Prettier integration
+{
+  plugins: {
+    prettier: require('eslint-plugin-prettier'),
   },
-
-  // Simple import sort
-  {
-    plugins: { 'simple-import-sort': require('eslint-plugin-simple-import-sort') },
-    rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-    },
+  rules: {
+    'prettier/prettier': 'error',
   },
-
-  // Custom TS/React rules
-  {
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'no-console': 'warn',
-    },
+}, // Simple import sort
+{
+  plugins: { 'simple-import-sort': require('eslint-plugin-simple-import-sort') },
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
-];
+}, // Custom TS/React rules
+{
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'warn',
+  },
+}, ...storybook.configs["flat/recommended"]];
 
 // import { dirname } from "path";
 // import { fileURLToPath } from "url";
